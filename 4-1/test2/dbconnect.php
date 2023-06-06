c9c5fe38790ce35e73201efb556be51edb2c5e18
mihotakahiro <tm0218@vesta.ocn.ne.jp>
2023-06-06 20:48:47 +0900

    課題提出テスト
    test1,test2の作成

---
+++ 4-1/test2/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+    // セッション開始
+    session_start();
+    // DBサーバのURL
+    $db['host'] = "C:\xampp\mysql\bin";
+    // ユーザー名
+    $db['user'] = "root";
+    // ユーザー名のパスワード
+    $db['pass'] = "root";
+    // データベース名
+    $db['dbname'] = "YIGroupBlog";
+?>
