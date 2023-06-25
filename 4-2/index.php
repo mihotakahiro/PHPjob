8ec43c6acc466aae8036cfd2f2ca3212e6ac9b5c
mihotakahiro <tm0218@vesta.ocn.ne.jp>
2023-06-25 18:57:46 +0900

    途中経過
    途中経過です

---
+++ 4-2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>divパズルlv2</title>
+    <link rel="stylesheet" href="reset.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="wrapper">
+        <div class="main clearfix">
+            <div class="header-left"><img src="1599315827_logo.png" width="200" height="120" alt="Y&I画像"></div>	
+            <div class="header-right-top"></div>	
+            <div class="header-right-bottom"></div>	
+        </div>		
+        <div class="main clearfix">
+
+            <div class="content">
+                <div class="kiji-ID">記事ID</div>
+                <div class="title">タイトル</div>
+
+            </div>
+        </div>
+
+        <div class="footer">Y&I group.inc</div>  
+    </div>	
+
+
+
+</body>
+</html>
